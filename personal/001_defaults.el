e9caf4e53c2dc586219abac9200276c90f897180
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2019-11-26 21:26:45 +0100

    modularized personal folder

---
+++ personal/001_defaults.el
@@ -0,0 +1,122 @@
+(prelude-require-packages '(
+                            anzu
+                            blacken
+                            csv-mode
+                            dockerfile-mode
+                            elfeed
+                            js2-mode
+                            json-mode
+                            go-mode
+                            go-eldoc
+                            go-projectile
+                            gotest
+                            magit
+                            markdown-mode
+                            protobuf-mode
+                            restclient
+                            tide
+                            typescript-mode
+                            prettier-js
+                            ))
+
+;; OS specific stuff
+(defvar gerlacdt/font "Inconsolata-16")
+
+(set-default 'default-frame-alist (\` ((font \, gerlacdt/font))))
+
+(when (eq system-type 'gnu/linux)
+  ;; do nothing
+  )
+
+;; OSX modifier keys and font
+(when (eq system-type 'darwin)
+  (setq mac-command-modifier 'meta
+        mac-option-modifier 'super
+        mac-right-option-modifier nil
+        gerlacdt/font "Menlo-16"))
+
+(when (eq window-system nil)
+  ;; needed ubuntu for line highlighting in emacs -nw mode (terminal)
+  (set-face-foreground 'highlight nil)
+  (set-face-background 'hl-line "#3e4446"))
+
+(when (window-system)
+  ;; do nothing
+  )
+
+(defun ms-windows? ()
+  "Check if current OS is Microsoft Windows."
+  (or (equal system-type 'cygwin) (equal system-type 'windows-nt)))
+
+(when (ms-windows?)
+  ;; flycheck does not work in ms-windows
+  (global-flycheck-mode -1))
+
+;; defines alias
+(defalias 'qrr 'anzu-query-replace-regexp)
+(defalias 'jpp 'json-pretty-print)
+(defalias 'ffp 'find-file-at-point)
+
+;; set some prelude defaults
+(setq prelude-guru nil)
+(setq prelude-flyspell nil)
+
+;; general stuff
+(set-scroll-bar-mode nil)
+(menu-bar-mode -1)
+(setq org-startup-folded nil)
+(setq tags-table-list nil)
+(setq compile-command "make")
+(setq line-number-display-limit-width 1000) ; show line number even if lines are really long
+(setq ediff-split-window-function 'split-window-horizontally)
+
+
+;; find grep
+(setq grep-find-command "find . -type f -print0 | xargs -0 grep -inH -e '' ")
+(setq grep-command "grep -inH -e '' ")
+(eval-after-load "grep"
+  '(progn
+     (add-to-list 'grep-find-ignored-directories "target")
+     (add-to-list 'grep-find-ignored-directories "node_modules")
+     (add-to-list 'grep-find-ignored-directories "vendor")))
+
+;; disable auto backups
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+;;gdb user interface with multiple windows
+(setq gdb-many-windows t)
+(setq gdb-use-separate-io-buffer t)
+(setq compilation-scroll-output t)
+
+;; helm
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+
+
+;; company-mode
+(global-set-key (kbd "C-<tab>") 'company-complete-common-or-cycle)
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+(setq company-dabbrev-downcase nil)
+(setq company-show-numbers t)
+
+;; projectile
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+;; projectile native indexing, much faster on windows
+(setq projectile-indexing-method 'alien)
+
+
+;; smartparens
+(setq sp-base-key-bindings 'paredit)
+(sp-use-paredit-bindings)
+
+;; smartparens for protobuf-mode
+(add-hook 'protobuf-mode-hook #'smartparens-mode)
+
+;; smartparens for haskell-interactive-mode-hook
+(add-hook 'haskell-interactive-mode-hook #'smartparens-mode)
---
+++ personal/002_functions.el
@@ -0,0 +1,76 @@
+;; custom functions
+
+;;split emacs in 3 windows with same height and width
+(defun gerlacdt/split3 ()
+  (interactive)
+  (split-window-horizontally)
+  (split-window-horizontally) (balance-windows))
+
+
+(defun gerlacdt/json-compress ($string &optional $from $to)
+  "Remove all spaces and newlines in the given string. When
+called interactively, work on current paragraph or text
+selection. When called in lisp code, if $string is non-nil,
+returns a changed string.  If $string nil, change the text in the
+region between positions $from $to."
+  (interactive
+   (if (use-region-p)
+       (list nil (region-beginning) (region-end))
+     (let ((bds (bounds-of-thing-at-point 'paragraph)))
+       (list nil (car bds) (cdr bds)))))
+
+  (let (workOnStringP inputStr outputStr)
+    (setq workOnStringP (if $string t nil))
+    (setq inputStr (if workOnStringP $string (buffer-substring-no-properties $from $to)))
+    (setq outputStr
+          (let ((case-fold-search t))
+            (replace-regexp-in-string "\s-*\\|\n" "" inputStr)))
+    (if workOnStringP
+        outputStr
+      (save-excursion
+        (delete-region $from $to)
+        (goto-char $from)
+        (insert outputStr)))))
+
+
+(defun gerlacdt/unfill-region (from to)
+  "Remove all newlines in the marked region FROM and TO.
+Replace them with a single space."
+  (interactive "r")
+  (let (inputStr outputStr)
+    (setq inputStr (buffer-substring-no-properties from to))
+    (setq outputStr (let ((case-fold-search t))
+                      (replace-regexp-in-string "\n" " " inputStr)))
+    (save-excursion
+      (delete-region from to)
+      (goto-char from)
+      (insert outputStr))))
+
+
+;; xml
+(defun gerlacdt/xml-pretty-print (point-min point-max)
+  "Formats the marked region in pretty-printed xml.
+POINT-MIN and POINT-MAX mark the region."
+  (interactive "r")
+  (save-excursion
+    (sgml-pretty-print (point-min) (point-max))
+    (indent-region (point-min) (point-max))))
+
+
+(defun gerlacdt/recompile-all-packages ()
+  "Recompile all installed ELPA packages. Needed if some package
+is deprecated and needs re-compilation."
+  (interactive)
+  (byte-recompile-directory package-user-dir 0 t))
+
+
+(defun gerlacdt/find-init-file ()
+  "Opens the current custom.el."
+  (interactive)
+  (find-file (expand-file-name "~/.emacs.d/personal/custom.el")))
+
+
+(defun gerlacdt/clear-packages ()
+  "Installs selectd packages and removes all unselected packages."
+  (package-install-selected-packages)
+  (package-autoremove))
