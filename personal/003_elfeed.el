6b9443804958aa09f1d269b1049223f46862769a
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2020-09-17 21:15:32 +0200

    cleaned up elfeed rss feeds

--- personal/003_elfeed.el
+++ personal/003_elfeed.el
@@ -2,25 +2,28 @@
 
 ;; data is stored in ~/.elfeed
 (setq elfeed-feeds
-      '(("https://www.reddit.com/r/programming.rss" programming)
-        ("https://www.reddit.com/r/emacs.rss" emacs)
-        ("https://www.reddit.com/r/golang.rss" golang)
-        ("https://www.reddit.com/r/java.rss" java)
-        ("https://www.reddit.com/r/scala.rss" scala)
-        ("https://www.reddit.com/r/javascript.rss" javascript)
-        ("https://www.reddit.com/r/typescript.rss" typescript)
-        ("https://www.reddit.com/r/aws.rss" aws)
-        ("https://www.reddit.com/r/googlecloud.rss" googlecloud)
-        ("https://www.reddit.com/r/devops.rss" devops)
-        ("https://www.reddit.com/r/azure.rss" azure)
-        ("https://www.reddit.com/r/kubernetes.rss" kubernetes)
-        ("https://www.reddit.com/r/lisp.rss" lisp)
-        ("https://www.reddit.com/r/clojure.rss" clojure)
-        ("https://www.reddit.com/r/python.rss" python)
-        ("https://www.reddit.com/r/kotlin.rss" kotlin)
+      '(
+        ;; programming
         ("https://news.ycombinator.com/rss" hacker)
         ("https://www.heise.de/developer/rss/news-atom.xml" heise)
-        ("https://martinfowler.com/feed.atom" mfowler)))
+        ("https://www.reddit.com/r/programming.rss" programming)
+        ("https://www.reddit.com/r/emacs.rss" emacs)
+
+        ;; programming languages
+        ("https://www.reddit.com/r/golang.rss" golang)
+        ("https://www.reddit.com/r/java.rss" java)
+        ("https://www.reddit.com/r/javascript.rss" javascript)
+        ("https://www.reddit.com/r/typescript.rss" typescript)
+        ("https://www.reddit.com/r/clojure.rss" clojure)
+        ("https://www.reddit.com/r/python.rss" python)
+
+        ;; cloud
+        ("https://www.reddit.com/r/aws.rss" aws)
+        ("https://www.reddit.com/r/googlecloud.rss" googlecloud)
+        ("https://www.reddit.com/r/azure.rss" azure)
+        ("https://www.reddit.com/r/devops.rss" devops)
+        ("https://www.reddit.com/r/kubernetes.rss" kubernetes)
+))
 
 (setq-default elfeed-search-filter "@2-days-ago +unread")
 (setq-default elfeed-search-title-max-width 100)
