b73801a0644edbe8fd174138ca23fa79607557fa
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2023-05-21 13:18:36 +0200

    removed treesitter from terraform-mode because it breaks sometimes the highlighting

--- personal/terraform.el
+++ personal/terraform.el
@@ -1,17 +1,10 @@
 (require 'prelude-programming)
 
-(prelude-require-packages '(terraform-mode
-                            tree-sitter
-                            tree-sitter-langs))
-
-(require 'tree-sitter)
-(require 'tree-sitter-langs)
+(prelude-require-packages '(terraform-mode))
 
 (with-eval-after-load 'terraform-mode
   (setq lsp-disabled-clients '(tfls)) ;; use hashicorp lsp server
   (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
-  (add-hook 'terraform-mode-hook #'tree-sitter-mode)
-  (add-hook 'terraform-mode-hook #'tree-sitter-hl-mode)
   (add-hook 'terraform-mode-hook 'lsp) ; enable lsp for terraform
 
 
