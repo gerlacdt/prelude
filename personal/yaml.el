449982b52d42e4db92462faf2ef0e01271b81a88
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2023-08-05 14:16:27 +0200

    moved to eglot for yaml

--- personal/yaml.el
+++ personal/yaml.el
@@ -5,28 +5,24 @@
 
 (prelude-require-packages '(yaml-mode))
 
-
-
 (with-eval-after-load 'yaml-mode
   (defun prelude-yaml-mode-defaults ()
-    ;; some config
-    )
+    ;; CamelCase aware editing operations
+    (subword-mode +1)
 
-  (add-hook 'yaml-mode-hook 'whitespace-mode)
-  (add-hook 'yaml-mode-hook 'subword-mode)
-  (add-hook 'yaml-mode-hook 'lsp)
+    ;; yaml-mode doesn't derive from prog-mode, but we can at least enable
+    ;; whitespace-mode and apply cleanup.
+    (whitespace-mode +1)
 
-  (add-hook 'yaml-mode-hook
-            (lambda () (add-hook 'before-save-hook 'prelude-cleanup-maybe nil t)))
+    (add-hook 'before-save-hook 'prelude-cleanup-maybe nil t))
+
 
   (setq prelude-yaml-mode-hook 'prelude-yaml-mode-defaults)
   (add-hook 'yaml-mode-hook (lambda ()
                               (run-hooks 'prelude-yaml-mode-hook)))
 
-  )
+  (add-hook 'yaml-mode-hook 'eglot-ensure))
 
-;; yaml-mode doesn't derive from prog-mode, but we can at least enable
-;; whitespace-mode and apply cleanup.
 
 (provide 'prelude-yaml)
 ;;; prelude-yaml.el ends here
